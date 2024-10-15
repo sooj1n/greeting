8329a9bb512373cb21392cd02545c9c9a26060fc
sooj1n <sujin19371@gmail.com>
2024-10-15 17:49:16 +0900

    name

--- src/main/java/shop/samdul/greeting/controller/greetingController.java
+++ src/main/java/shop/samdul/greeting/controller/greetingController.java
@@ -1,6 +1,6 @@
 package shop.samdul.greeting.controller;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
