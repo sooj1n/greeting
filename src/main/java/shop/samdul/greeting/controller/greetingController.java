f179ae01bcb47561b5520d03a8c57b4307633c9a
sooj1n <sujin19371@gmail.com>
2024-10-16 10:06:30 +0900

    이전내용 저장하기

--- src/main/java/shop/samdul/greeting/controller/greetingController.java
+++ src/main/java/shop/samdul/greeting/controller/greetingController.java
@@ -1,13 +1,15 @@
 package shop.samdul.greeting.controller;
 
+import java.util.Stack;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class greetingController {
-
+    Stack<String> names = new Stack<>();
     @GetMapping("/greeting")
     public String greeting( @RequestParam(
             name="name",
@@ -15,7 +17,8 @@ public class greetingController {
             defaultValue = "HI") String name,
             Model model) {
 
-        model.addAttribute("name", name);
+        names.push(name);
+        model.addAttribute("names", names);
 
         return "greeting";
     }
