d8999697cc4bfa23fa048ca0b22a8a8182df6a9a
sooj1n <sujin19371@gmail.com>
2024-10-22 16:22:33 +0900

    service

---
+++ src/main/java/shop/samdul/greeting/entity/TodoEntity.java
@@ -0,0 +1,17 @@
+package shop.samdul.greeting.entity;
+
+import lombok.Setter;
+import lombok.Getter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class TodoEntity {
+    private Integer id;
+    private String subject;
+    private String body;
+    private Boolean completed;
+}
+
+
