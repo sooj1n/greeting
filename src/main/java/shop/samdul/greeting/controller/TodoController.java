8e8f0140ab93c1d89e6d4c816304bc23b23cb2c8
sooj1n <sujin19371@gmail.com>
2024-10-23 09:28:59 +0900

    20241022

---
+++ src/main/java/shop/samdul/greeting/controller/TodoController.java
@@ -0,0 +1,27 @@
+package shop.samdul.greeting.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import shop.samdul.greeting.entity.TodoEntity;
+import shop.samdul.greeting.service.TodoService;
+
+import java.util.List;
+
+public class TodoController {
+
+    @Autowired
+    TodoService todoService;
+
+    @GetMapping("/list")
+    public List<TodoEntity> list(){
+        System.out.println("[Controller]");
+        List<TodoEntity> r = todoService.getTodos();
+        return r;
+    }
+    @GetMapping("/todos/{id}")
+    public TodoEntity find(@PathVariable Integer id){
+        TodoEntity r = todoService.findById(id);
+        return r;
+    }
+}
